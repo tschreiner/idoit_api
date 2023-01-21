ed822e186b098762d0cb141228389e36299abd7f
Tedd Schreiner <info@teddschreiner.de>
2023-01-21 17:20:55 +0100

    Add idoit_api_client module and idoit_api_client.Constants tests

--- tests/test_idoit_api.py
+++ tests/test_idoit_api.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
-
 """Tests for `idoit_api_client` package."""
 
 import pytest
-
 from click.testing import CliRunner
-
 from idoit_api_client import idoit_api_client
+
+from idoit_api_client.idoit_api_client import Constants, API
 from idoit_api_client import cli
 
 
 @pytest.fixture
 def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """Sample pytest fixture.    See more at: http://doc.pytest.org/en/latest/fixture.html
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
@@ -25,6 +22,29 @@ def test_content(response):
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
+def test_constants():
+    """Test constants."""
+    constants =  Constants
+    assert constants.URL == 'url'
+    assert constants.PORT == 'port'
+    assert constants.PORT_MIN == 1
+    assert constants.PORT_MAX == 65535
+    assert constants.KEY == 'key'
+    assert constants.USERNAME == 'username'
+    assert constants.PASSWORD == 'password'
+    assert constants.LANGUAGE == 'language'
+    assert constants.PROXY == 'proxy'
+    assert constants.PROXY_ACTIVE == 'active'
+    assert constants.PROXY_TYPE == 'type'
+    assert constants.PROXY_TYPE_HTTP == 'HTTP'
+    assert constants.PROXY_TYPE_SOCKS5 == 'SOCKS5'
+    assert constants.PROXY_HOST == 'host'
+    assert constants.PROXY_PORT == 'port'
+    assert constants.PROXY_USERNAME == 'username'
+    assert constants.PROXY_PASSWORD == 'password'
+    assert constants.BYPASS_SECURE_CONNECTION == 'bypassSecureConnection'
+    assert constants.CURLPROTO_HTTP == 1
+    assert constants.CURLPROTO_HTTPS == 2
 
 def test_command_line_interface():
     """Test the CLI."""
